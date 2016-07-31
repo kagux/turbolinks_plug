f52711dc468351c31abc7e7574cb59cd6b565289
Boris Mikhaylov <kaguxmail@gmail.com>
2016-07-31 22:19:17 +0300

    added plug dep, updated project description and added license

--- mix.exs
+++ mix.exs
@@ -2,31 +2,46 @@ defmodule TurbolinksPlug.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :turbolinks_plug,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :turbolinks_plug,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package,
+      description: description,
+      docs: [
+        extras: ~W(README.md)
+      ]
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:plug, "~> 1.1"}
+    ]
+  end
+
+   defp description do
+    """
+    An elixir plug that adds headers on redirects to hint turbolinks correct URL
+    """
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README.md LICENSE),
+      maintainers: ["Boris Mikhaylov"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "http://github.com/kagux/turbolinks_plug",
+        "Docs"   => "http://hexdocs.pm/turbolinks_plug",
+      }
+    ]
   end
 end
