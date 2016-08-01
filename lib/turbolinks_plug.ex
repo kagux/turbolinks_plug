522b3a99997169b5e0bcad91c68e8fb35b5d06f2
Boris Mikhaylov <kaguxmail@gmail.com>
2016-08-01 18:58:15 +0300

    301 session test

--- lib/turbolinks_plug.ex
+++ lib/turbolinks_plug.ex
@@ -1,2 +1,42 @@
 defmodule TurbolinksPlug do
+  import Plug.Conn
+
+  @session_key "_turbolinks_location"
+  @location_header "Turbolinks-Location"
+  @referrer_header "turbolinks-referrer"
+
+  def init(opts), do: opts
+
+  def call(conn, _) do
+    register_before_send(conn, &handle_redirect/1)
+  end
+
+  def handle_redirect(%Plug.Conn{status: s} = conn) when s == 301 or s == 302 do
+    location = get_resp_header(conn, "location")
+    referrer = get_resp_header(conn, @referrer_header)
+    store_location_in_session(conn, location, referrer)
+  end
+
+  def handle_redirect(%Plug.Conn{status: s} = conn) when s >= 200 and s < 300  do
+    conn
+     |> get_session(@session_key)
+     |> hd
+     |> set_location_header(conn)
+  end
+
+  def handle_redirect(conn), do: conn
+
+  defp store_location_in_session(conn, _, nil), do: conn
+
+  defp store_location_in_session(conn, location, _referrer) do
+    put_session(conn, @session_key, location)
+  end
+
+  defp set_location_header(nil, conn), do: conn
+  
+  defp set_location_header(location, conn) do
+    conn 
+      |> put_resp_header(@location_header, location) 
+      |> delete_session(@session_key)
+  end
 end
--- test/turbolinks_plug_test.exs
+++ test/turbolinks_plug_test.exs
@@ -1,8 +1,25 @@
 defmodule TurbolinksPlugTest do
   use ExUnit.Case
-  doctest TurbolinksPlug
+  use Plug.Test
+  import Plug.Conn
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it stores redicted url in session" do
+    session = Plug.Session.init(
+      store: :ets,
+      key: "_test_session",
+      table: :session
+    )
+    :ets.new(:session, [:named_table, :public, read_concurrency: true])
+    url_in_session =  conn(:get, "/")
+                      |> Plug.Session.call(session)
+                      |> fetch_session
+                      |> put_req_header("turbolinks-referrer", "http://example.com")
+                      |> put_resp_header("location", "/new-url")
+                      |> TurbolinksPlug.call(%{})
+                      |> send_resp(302, "")
+                      |> get_session("_turbolinks_location")
+                      |> hd
+
+    assert url_in_session == "/new-url"
   end
 end
