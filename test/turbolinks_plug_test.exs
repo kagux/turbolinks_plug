23fcb63757927327f2cfe390c3a836f93c54dec8
Boris Mikhaylov <kaguxmail@gmail.com>
2016-08-02 12:46:42 +0300

    cleaned up tests

--- test/turbolinks_plug_test.exs
+++ test/turbolinks_plug_test.exs
@@ -21,9 +21,7 @@ defmodule TurbolinksPlugTest do
   test "it stores redirected url in session", %{conn: conn} do
     url_in_session = conn
                       |> put_req_header("turbolinks-referrer", "http://example.com")
-                      |> put_resp_header("location", "/new-url")
-                      |> TurbolinksPlug.call(%{})
-                      |> send_resp(302, "")
+                      |> send_resp_with_location_header(status: 302, location: "/new-url")
                       |> get_session("_turbolinks_location")
 
     assert url_in_session == "/new-url"
@@ -31,9 +29,7 @@ defmodule TurbolinksPlugTest do
 
   test "it does not store redirected url if referrer is missing", %{conn: conn} do
     url_in_session = conn
-                      |> put_resp_header("location", "/new-url")
-                      |> TurbolinksPlug.call(%{})
-                      |> send_resp(302, "")
+                      |> send_resp_with_location_header(status: 302, location: "/new-url")
                       |> get_session("_turbolinks_location")
 
     refute url_in_session
@@ -41,9 +37,7 @@ defmodule TurbolinksPlugTest do
 
   test "it sets turbolinks header if location present in session", %{conn: conn} do
     header = conn
-              |> put_session("_turbolinks_location", "/redirected")
-              |> TurbolinksPlug.call(%{})
-              |> send_resp(200, "")
+              |> send_resp_with_location_in_session(status: 200)
               |> get_resp_header("turbolinks-location")
               |> hd
 
@@ -52,9 +46,7 @@ defmodule TurbolinksPlugTest do
 
   test "it sets turbolinks header only for OK responsesl", %{conn: conn} do
     header = conn
-              |> put_session("_turbolinks_location", "/redirected")
-              |> TurbolinksPlug.call(%{})
-              |> send_resp(404, "")
+              |> send_resp_with_location_in_session(status: 404)
               |> get_resp_header("turbolinks-location")
 
     assert header == []
@@ -62,11 +54,27 @@ defmodule TurbolinksPlugTest do
 
   test "it removes turbolinks location from session after setting header", %{conn: conn} do
     url_in_session = conn
-                      |> put_session("_turbolinks_location", "/redirected")
-                      |> TurbolinksPlug.call(%{})
-                      |> send_resp(200, "")
+                      |> send_resp_with_location_in_session(status: 200)
                       |> get_session("_turbolinks_location")
 
     refute url_in_session
+  end
+
+  defp send_resp_with_location_in_session(conn, opts) do
+    conn
+    |> put_session("_turbolinks_location", "/redirected")
+    |> send_resp_with_turbolinks(opts)
+  end
+
+  defp send_resp_with_location_header(conn, opts) do
+    conn
+    |> put_resp_header("location", opts[:location])
+    |> send_resp_with_turbolinks(opts)
+  end
+
+  defp send_resp_with_turbolinks(conn, opts) do
+    conn
+    |> TurbolinksPlug.call(%{})
+    |> send_resp(opts[:status], "")
   end
 end
