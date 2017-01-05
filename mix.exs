cb5b397571f7ab6163e1ffb895a92ed738dd40d5
Boris Mikhaylov <kaguxmail@gmail.com>
2017-01-05 14:58:12 +0100

    release 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TurbolinksPlug.Mixfile do
   def project do
     [
       app: :turbolinks_plug,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
