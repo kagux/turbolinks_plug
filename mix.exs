14704f36c43b99e0545859ad49096e5b7f78d2c4
Boris Mikhaylov <kaguxmail@gmail.com>
2018-07-09 15:21:50 +0200

    bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TurbolinksPlug.Mixfile do
   def project do
     [
       app: :turbolinks_plug,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
